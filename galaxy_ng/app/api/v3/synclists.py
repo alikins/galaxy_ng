6812408142431a09de187fb500536631aa645b44
Adrian Likins <alikins@redhat.com>
2020-03-30 15:49:17 -0400

    Allow put/post of synclists with empty namespaces or collections

--- galaxy_ng/app/api/v3/synclists.py
+++ galaxy_ng/app/api/v3/synclists.py
@@ -2,16 +2,21 @@ import logging
 
 from rest_framework import serializers
 
+from pulp_ansible.app import models as pulp_ansible_models
+
 from galaxy_ng.app.api import base as api_base
-from galaxy_ng.app.api import permissions
 from galaxy_ng.app import models
-# from galaxy_ng.app.api.v3 import serializers
 
 
 log = logging.getLogger(__name__)
 
 
 class SyncListSerializer(serializers.ModelSerializer):
+    namespaces = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
+                                                    queryset=models.Namespace.objects.all())
+    collections = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
+                                                     queryset=pulp_ansible_models.Collection.objects.all())
+
     class Meta:
         model = models.SyncList
         fields = ['id', 'name', 'policy',
@@ -21,5 +26,3 @@ class SyncListSerializer(serializers.ModelSerializer):
 class SyncListViewSet(api_base.ModelViewSet):
     queryset = models.SyncList.objects.all()
     serializer_class = SyncListSerializer
-
-
