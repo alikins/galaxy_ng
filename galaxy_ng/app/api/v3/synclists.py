400926b882ce9956edd960f939189284394d6236
Adrian Likins <alikins@redhat.com>
2020-04-03 09:58:45 -0400

    Add serializers for SyncListCollection

--- galaxy_ng/app/api/v3/synclists.py
+++ galaxy_ng/app/api/v3/synclists.py
@@ -11,11 +11,46 @@ from galaxy_ng.app import models
 log = logging.getLogger(__name__)
 
 
+class SyncListCollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = pulp_ansible_models.Collection
+        fields = ['namespace', 'name']
+
+
 class SyncListSerializer(serializers.ModelSerializer):
-    namespaces = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
-                                                    queryset=models.Namespace.objects.all())
-    collections = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
-                                                     queryset=pulp_ansible_models.Collection.objects.all())
+    # namespaces = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
+    #                                                queryset=models.Namespace.objects.all())
+    namespaces = serializers.SlugRelatedField(many=True, slug_field='name',
+                                              queryset=models.Namespace.objects.all())
+
+    # collections = serializers.PrimaryKeyRelatedField(many=True, allow_null=True,
+    #                                                 queryset=onsible_models.Collection.objects.all())
+
+    collections = SyncListCollectionSerializer(many=True)
+    # collections = serializers.SlugRelatedField(many=True, slug_field='name',
+    #                                           allow_null=True,
+    #                                           source='*',
+    #                                           queryset=pulp_ansible_models.Collection.objects.all())
+
+    def get_collection_label(self):
+        pass
+
+    def create(self, validated_data):
+        collections_data = validated_data.pop('collections')
+        log.debug('collections_data: %s', collections_data)
+
+        namespaces_data = validated_data.pop('namespaces')
+        log.debug('ns_data: %s', namespaces_data)
+
+        instance = models.SyncList.objects.create(**validated_data)
+        for collection_data in collections_data:
+            instance.collections.add(pulp_ansible_models.Collection.objects.get(**collection_data))
+
+        for namespace_data in namespaces_data:
+            log.debug('namespace_data: %s %s', namespace_data, type(namespace_data))
+            # instance.namespaces.add(models.Namespace.objects.get(**namespace_data))
+            instance.namespaces.add(namespace_data)
+        return instance
 
     class Meta:
         model = models.SyncList
