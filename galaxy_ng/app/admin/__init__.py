97d6eed80a7452eace672746907e4f2630a638c5
Adrian Likins <alikins@redhat.com>
2020-05-18 11:07:43 -0400

    Add ModelAdmin objects for galaxy_ng
    Use pulpcore.admin.BaseModel for guardian obj perms
    
    Use pulpcore AdminModels from plugins package.
    
    Add pulp_container admin models.
    
    Add galaxy container admin models.
    
    Utility formatters for collection import for use
    in admin.
    
    Update check_pulpcore_imports to ignore admin/
    
    Issue: AAH-32

---
+++ galaxy_ng/app/admin/__init__.py
@@ -0,0 +1,19 @@
+from . import base
+from . import pulp_container
+from . import content_type
+from . import galaxy
+from . import guardian
+from . import permission
+from . import pulp_ansible
+from . import pulpcore
+
+__all__ = (
+    'base',
+    'pulp_container',
+    'content_type',
+    'galaxy',
+    'guardian',
+    'permission',
+    'pulp_ansible',
+    'pulpcore',
+)
