0b3f0b92b7b149ed617d856461d6ea829d28cfe7
Adrian Likins <alikins@redhat.com>
2020-05-19 17:30:43 -0400

    Import namespace viewsets into v3 viewsets

--- galaxy_ng/app/api/v3/viewsets/__init__.py
+++ galaxy_ng/app/api/v3/viewsets/__init__.py
@@ -6,10 +6,18 @@ from .collection import (
     CollectionVersionViewSet,
 )
 
+from .namespace import (
+    MyNamespaceViewSet,
+    NamespaceViewSet,
+)
+
+
 __all__ = (
     'CollectionArtifactDownloadView',
     'CollectionUploadViewSet',
     'CollectionViewSet',
     'CollectionVersionViewSet',
     'CollectionImportViewSet',
+    'NamespaceViewSet',
+    'MyNamespaceViewSet',
 )
