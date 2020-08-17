ca0ad3588637d26435e4d747a1750f6f18576f18
Adrian Likins <alikins@redhat.com>
2020-08-17 19:34:39 -0400

    Include the namespace spec'ed cv's in the synclist repo
    Refactor and simplify curate_synclist_repository
    
    Issue: #265
    Issue: #285
    Issue: #17

--- galaxy_ng/app/tasks/synclist.py
+++ galaxy_ng/app/tasks/synclist.py
@@ -2,7 +2,7 @@ import logging
 import pprint
 import time
 
-from django.db.models import F
+from django.db.models import F, Subquery
 
 from pulpcore.app.models import (
     CreatedResource,
@@ -72,12 +72,6 @@ def curate_all_synclist_repository(upstream_repository_name, **kwargs):
                                  total=synclist_qs.count()) as task_progress_report:
 
         for synclist in synclist_qs:
-            log.debug("curating synclist repo %s from upstream repo %s",
-                      synclist, upstream_repository)
-            log.debug("synclist %s policy=%s", synclist, synclist.policy)
-            log.debug("sycnlist %s collections=%s", synclist, synclist.collections.all())
-            log.debug("synclist %s namespaces=%s", synclist, synclist.namespaces.all())
-
             # TODO: filter down to just synclists that have a synclist repo
             # locks need to be Model or str not int
             locks = [str(synclist.id)]
@@ -117,104 +111,47 @@ def curate_synclist_repository(synclist_pk, **kwargs):
     This is intended to work on one synclist and synclist repo at a time.
     """
 
-    # Lookup synclist by synclist_pk, which will include the
-    # synclist repo and the upstream_repo.
-
-    # For 'include' policy cases, move the latest version of the specified collections to
-    # the synclist repos.
-    #   - Apply the collection 'specs' to the upstream repository version to
-    #     filter down to candidates
-
-    # For 'exclude' policy cases, updated everything except for the excludes.
-
-    # moving/updating means creating at least a 'added' and 'removed' delta and using
-    # repository /move (ideally in as few steps as possible to limit number RepoVersions
-
-    # This likely means enqueue galaxy.app.tasks.promotion.[add|remote]_content_to_repository
-    # tasks. Or possible pulpcore.app.tasks.repository.add_and_remove()
-
-    # Also see pulp_ansible.app.tasks.test_tasks for examples of tasks that operate over many repos
-    # and pulpcore.app.tasks.import for examples of tasks that work on lots of Content
-
-    # And pulp_ansible.tests.performance
-    task_start = time.time()
-
-    log.debug("synclist_pk: %s", synclist_pk)
-    log.debug("kwargs: %r", kwargs)
-
     synclist = models.SyncList.objects.get(pk=synclist_pk)
-    log.debug('synclist instance: %s', synclist)
 
     upstream_repository = synclist.upstream_repository
+    latest_upstream = upstream_repository.latest_version()
 
-    log.debug("curating synclist repo %s from %s", synclist, upstream_repository)
-    log.debug("synclist %s policy=%s", synclist, synclist.policy)
-    log.debug("synclist %s collections(repr)=%r", synclist, synclist.collections)
-    log.debug("synclist %s collections.all()=%s", synclist, synclist.collections.all())
-    log.debug("synclsit %s collections.count=%s", synclist, synclist.collections.count())
-    log.debug("synclist %s namespaces=%s", synclist, synclist.namespaces.all())
-
-    log.debug('Task finished after %s seconds', time.time() - task_start)
-    log.debug('Task finished after %s seconds', time.time() - task_start)
-
-    # locks need to be Model or str not int
-    log.debug('synclist.repository: %s', synclist.repository)
+    log.info(
+        'Applying synclist "%s" with policy=%s to curate repo "%s" from upstream repo "%s"',
+        synclist.name,
+        synclist.policy,
+        synclist.repository.name,
+        upstream_repository.name,
+    )
 
     locks = [synclist.repository]
 
-    # FIXME: add enum constants to models.SyncList
+    namespaces = synclist.namespaces.filter().values_list('name', flat=True)
+
+    collection_versions = \
+        CollectionVersion.objects.filter(repositories=synclist.upstream_repository,
+                                         collection__namespace__in=namespaces,
+                                         is_highest=True) \
+        & CollectionVersion.objects.filter(collection__in=synclist.collections.all(),
+                                           repositories=synclist.repository)
+
     if synclist.policy == 'exclude':
-        latest_upstream = upstream_repository.latest_version()
-
-        log.debug('latest_upstream: %s %s', latest_upstream, type(latest_upstream))
-
-        excluded_content_pks = \
-            CollectionVersion.objects.filter(collection_id__in=synclist.collections.all(),
-                                             is_highest=True,
-                                             repositories=synclist.upstream_repository)
-
-        log.debug('excluded_content_pks:\n%s',
-                  pf([x.natural_key() for x in excluded_content_pks]))
-
-        task_kwargs = {'base_version_pk': str(upstream_repository.latest_version().pulp_id),
+        task_kwargs = {'base_version_pk': str(latest_upstream.pulp_id),
                        'repository_pk': str(synclist.repository.pulp_id),
                        'add_content_units': [],
-                       'remove_content_units': excluded_content_pks,
+                       'remove_content_units': collection_versions,
                        }
 
-        log.debug('task_kwargs: %s', pf(task_kwargs))
-
-        subtask = enqueue_with_reservation(add_and_remove,
-                                           locks,
-                                           kwargs=task_kwargs,
-                                           )
-
-        log.debug('subtask(exclude): %s', subtask)
-
-        return
-
-    if synclist.policy == 'include':
-        latest_upstream = upstream_repository.latest_version()
-
-        added_all_content_pks = \
-            CollectionVersion.objects.filter(collection_id__in=synclist.collections.all(),
-                                             is_highest=True,
-                                             repositories=synclist.upstream_repository)
-
-        log.debug('added_all_content_pks:\n%s',
-                  pf([x.natural_key() for x in added_all_content_pks]))
-
+    elif synclist.policy == 'include':
         task_kwargs = {'repository_pk': str(synclist.repository.pulp_id),
-                       'add_content_units': added_all_content_pks,
-                       # remove all existing content before adding add_content_units
+                       'add_content_units': collection_versions,
                        'remove_content_units': ['*']}
 
-        log.debug('task_kwargs: %s', task_kwargs)
+    log.debug('task_kwargs: %s', pf(task_kwargs))
 
-        subtask = enqueue_with_reservation(add_and_remove,
-                                           locks,
-                                           kwargs=task_kwargs,
-                                           )
+    subtask = enqueue_with_reservation(add_and_remove,
+                                       locks,
+                                       kwargs=task_kwargs,
+                                       )
 
-        log.debug('subtask(include): %s', subtask)
-        return
+    log.debug('subtask(exclude): %s', subtask)
